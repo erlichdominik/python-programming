ca83ce275e47ed19398e9374fbf6dae338a89fda
mimikdesu <mimikdesu@gmail.com>
2021-04-02 15:40:53 +0200

    modify chapter1.py

--- data_science/courses/data_science_from_beginning_oreilly/chapter1.py
+++ data_science/courses/data_science_from_beginning_oreilly/chapter1.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from collections import defaultdict
+
 users = [
     {"id": 0, "name": "Hero"},
     {"id": 1, "name": "Dunn"},
@@ -14,26 +17,76 @@ users = [
 friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
                     (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
+# directory of user_id : array of friends
 friendships = {user["id"]: [] for user in users}
 
 for i, j in friendship_pairs:
     friendships[i].append(j)
     friendships[j].append(i)
 
+
 def number_of_friends(user):
     user_id = user["id"]
     friend_ids = friendships[user_id]
     return len(friend_ids)
+
 
 total_connections = sum(number_of_friends(user) for user in users)
 
 num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
 
 num_friends_by_id.sort(
-    key = lambda id_and_friends: id_and_friends[1],
-    reverse = True
+    key=lambda id_and_friends: id_and_friends[1],
+    reverse=True
 )
 
+print(friendships)
 
 
-print(num_friends_by_id)
+# finding friends of friend for input user
+def foaf_ids_bad(user):
+    return [foaf_id for friend_id in friendships[user["id"]]
+            for foaf_id in friendships[friend_id]]
+
+
+def friends_of_friend_ids(user):
+    user_id = user["id"]
+    return Counter(
+        foaf_id
+        for friend_id in friendships[user_id]
+        for foaf_id in friendships[friend_id]
+        if foaf_id != user_id and foaf_id not in friendships[user_id]
+    )
+
+print(friends_of_friend_ids(users[0]))
+
+
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+# searching whole collection every time
+def data_scientists_who_like(target_interest):
+    return [user_id for user_id, user_interests in interests
+            if user_interests == target_interest]
+
+# better to to default dict
+user_ids_by_interest = defaultdict(list)
+
+for user_id, interest in interests:
+    user_ids_by_interest[user_id].append(interest)
+
+print(user_ids_by_interest)
